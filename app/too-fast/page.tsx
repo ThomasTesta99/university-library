ef3704304ee207a52d586aaa13911750d1da2423
Thomas Testa <ttesta99@yahoo.com>
2025-01-13 15:38:25 -0500

    initial commit

---
+++ app/too-fast/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className = "root-container flex min-h-screen flex-col items-center justify-center">
+        <h1 className = "font-bebas-neue text-5xl font-bold text-light-100">
+            Way too fast, slow down
+        </h1>
+        <p className = "mt-3 max-w-xl text-center text-light-400">
+            Too many requests can hurt our website. Take a minute to relax, then come back to explore.
+        </p>
+    </main>
+  )
+}
+
+export default page
